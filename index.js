2fd7fe9bca6333cf88e7c0856b1027232e9cfe63
Anthony Tepach <anthony.tepach@gmail.com>
2023-04-05 08:53:59 -0600

    factorizar codigo

--- index.js
+++ index.js
@@ -5,19 +5,12 @@ import fs from "fs";
 
 // URL de la API
 // URL de la imagen de fondo
-const imageUrl = "./fondo.jpg";
-let tipo = "QUINCENA";
-let fecha_inicio = "2023-03-23";
-let fecha__fin = "2023-03-30";
-
-const apiUrl = `http://192.168.1.8:3000/api/consulta?departamento=${tipo}&fecha_inicio=${fecha_inicio}&fecha_fin=${fecha__fin}`;
+const BACKGROUND_IMAGE_PATH = "./fondo.jpg";
 
 function imageToBase64(filePath) {
   const imageData = fs.readFileSync(filePath);
-  const base64 = Buffer.from(imageData).toString("base64");
-  return base64;
+  return imageData.toString("base64");
 }
-
 function getMonthName(dateString) {
   const date = new Date(dateString);
   const arr = dateString.split("-");
@@ -30,108 +23,122 @@ function getMonthName(dateString) {
     arr[0]
   );
 }
+function addBackgroundImage(doc, image_path) {
+  if (image_path && fs.existsSync(image_path)) {
+    doc.addPage();
+    doc.addImage(
+      imageToBase64(image_path),
+      "JPEG",
+      0,
+      0,
+      doc.internal.pageSize.width,
+      doc.internal.pageSize.height
+    );
+  }
+}
+function addEmployeeInfo(doc, employee, startDate, endDate) {
+  doc.text(6, 0.7, "N° Empleado: " + employee.emp_code);
+  doc.text(6, 1, employee.position_name);
+  doc.text(6, 1.3, employee.dept_name);
 
-// Realizar petición GET a la API y obtener información en formato JSON
-axios
-  .get(apiUrl)
-  .then((response) => {
-    const data = response.data;
-    //creacion de DOC
-    var doc = new jsPDF({
-      orientation: "portrait",
-      unit: "in",
-      format: [8.5, 11],
-    });
-  
+  doc.text(
+    0.6,
+    1.6,
+    "Nombre: " + employee.first_name + " " + employee.last_name
+  );
+  doc.setFontSize(12);
+  doc.text(
+    0.6,
+    2,
+    "Registros del: " + getMonthName(startDate) + " al " + getMonthName(endDate)
+  );
+  doc.setFontSize(16);
+}
 
-    data.forEach((employee, index) => {
-      for (let i = 1; i <= 2; i++) {
-       if (index > -1) {
-          doc.addPage();
-          doc.addImage(
-            imageToBase64("./fondo.jpg"),
-            "JPEG",
-            0,
-            0,
-            doc.internal.pageSize.width,
-            doc.internal.pageSize.height
-          );
-        }
+function addConformityLegend(doc) {
+  doc.setFontSize(10);
+  const text =
+    "El trabajador confirma que la impresión de su registro de entradas y salidas coincide fielmente con su horario de asistencias registrado digitalmente. Por lo tanto, ratifica el presente registro firmando de conformidad.";
+  const lines = doc.splitTextToSize(text, 5); // dividir en líneas de 80 caracteres de ancho
+  doc.text(lines, 2.5, 9.6);
+  doc.setFontSize(16);
+}
+function addEmployeePhoto(doc, emp_code) {
+  let filePath = `./fotos/${emp_code}.png`;
+  let exist = fs.existsSync(filePath)
+    ? filePath
+    : "./fotos/avatar-de-perfil.png";
 
-        doc.text(6, 0.7, "N° Empleado: " + employee.emp_code);
-        doc.text(6, 1, employee.position_name);
-        doc.text(6, 1.3, employee.dept_name);
+  doc.addImage(
+    imageToBase64(exist),
+    "PNG",
+    0.8,
+    doc.internal.pageSize.width,
+    1.5,
+    1.5
+  );
+}
+function addEmployeeSignature(employee, doc) {
+  doc.setFontSize(12);
+  doc.text(2.5, 9, `${employee.first_name} ${employee.last_name}`);
+  doc.setLineWidth(0.02); // Grosor de la línea
+  doc.line(2.5, 9.1, 7, 9.1);
+  doc.text(2.5, 9.3, "Nombre y Firma del Trabajador");
+  doc.setFontSize(16);
+}
 
-        doc.text(
-          0.6,
-          1.6,
-          "Nombre: " + employee.first_name + " " + employee.last_name
-        );
-        doc.setFontSize(12);
-        doc.text(
-          0.6,
-          2,
-          "Registros del: " +
-            getMonthName(fecha_inicio) +
-            " al " +
-            getMonthName(fecha__fin)
-        );
-        doc.setFontSize(16);
+function addPunchTimesTable(doc, employee, startY) {
+  const headers_punch_time = [["Fecha", "Hora", "Dispositivo"]];
+  const data_punch_time = employee.punch_times.map((punch) => [
+    getMonthName(punch.fecha),
+    punch.hora,
+    punch.checo_en,
+  ]);
 
-        //foto empleado
-
-        let filePath = `./fotos/${employee.emp_code}.png`;
-        var exist = "";
-        if (fs.existsSync(filePath)) {
-          exist = filePath;
-        } else {
-          exist = "./fotos/avatar-de-perfil.png";
-        }
-        doc.addImage(
-          imageToBase64(exist),
-          "PNG",
-          0.8,
-          doc.internal.pageSize.width,
-          1.5,
-          1.5
-        );
-        //Firma del empleado
-        doc.setFontSize(12);
-        doc.text(2.5, 9, employee.first_name + " " + employee.last_name);
-        doc.setLineWidth(0.02); // Grosor de la línea
-        doc.line(2.5, 9.1, 7, 9.1);
-        doc.text(2.5, 9.3, "Nombre y Firma del Trabajador");
-        doc.setFontSize(16);
-
-        //leyenda de conformidad
-        doc.setFontSize(10);
-        var text =
-          "El trabajador confirma que la impresión de su registro de entradas y salidas coincide fielmente con su horario de asistencias registrado digitalmente. Por lo tanto, ratifica el presente registro firmando de conformidad.";
-        var lines = doc.splitTextToSize(text, 5); // dividir en líneas de 80 caracteres de ancho
-        doc.text(lines, 2.5, 9.6);
-        doc.setFontSize(16);
-
-        var headers_punch_time = [["Fecha", "Hora", "Dispositivo"]];
-        var data_punch_time = [];
-        employee.punch_times.forEach(function (punch) {
-          data_punch_time.push([
-            getMonthName(punch.fecha),
-            punch.hora,
-            punch.checo_en,
-          ]);
-        });
-
-        doc.autoTable({
-          head: headers_punch_time,
-          body: data_punch_time,
-          startY: 2.1,
-          startX: 0,
-        });
-      }
-    });
-    doc.deletePage(1);
-    doc.save(`REPORTE_${tipo}_${fecha_inicio}_${fecha__fin}.pdf`);
-  })
-  .catch((error) => {
-    console.error(error);
+  doc.autoTable({
+    head: headers_punch_time,
+    body: data_punch_time,
+    startY: startY,
+    startX: 0,
   });
+}
+function getReport(REPORT_TYPE, START_DATE, END_DATE) {
+  axios
+    .get("http://192.168.1.8:3000/api/consulta", {
+      params: {
+        departamento: REPORT_TYPE,
+        fecha_inicio: START_DATE,
+        fecha_fin: END_DATE,
+      },
+    })
+    .then((response) => {
+      const data = response.data;
+      const doc = new jsPDF({
+        orientation: "portrait",
+        unit: "in",
+        format: [8.5, 11],
+      });
+
+      data.forEach((employee,index) => {
+        addBackgroundImage(doc, BACKGROUND_IMAGE_PATH);
+        addEmployeeInfo(doc, employee, START_DATE, END_DATE);
+        addEmployeePhoto(doc, employee.emp_code);
+        addEmployeeSignature(employee, doc);
+        addConformityLegend(doc);
+        addPunchTimesTable(doc, employee, 2.1);
+       
+      });
+      doc.deletePage(1)
+      doc.save(`REPORTE_${REPORT_TYPE}_${START_DATE}_${END_DATE}.pdf`);
+
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+
+
+getReport("QUINCENA", "2023-03-23", "2023-03-30");
+
+
